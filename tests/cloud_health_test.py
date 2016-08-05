0644c8cfeda3bd0db2822eef184f58b0c4c91537
Kareena Hirani <khirani@krux.com>
2016-08-05 15:09:37 -0700

    Fixed cloud_health_test.py except get_data and get_data_info

--- tests/cloud_health_test.py
+++ tests/cloud_health_test.py
@@ -31,14 +31,20 @@ class CloudHealthTest(unittest.TestCase):
     API_KEY = '12345'
     COST_HISTORY_REPORT = 'olap_reports/cost/history'
     COST_CURRENT_REPORT = 'olap_reports/cost/current'
+    API_ENDPOINT = 'https://apps.cloudhealthtech.com/'
+    COST_HISTORY_URI = '{}{}'.format(API_ENDPOINT, COST_HISTORY_REPORT)
+    URI_ARGS_NO_PARAMS = {'api_key': API_KEY}
+    TIME_INTERVAL = Interval['daily']
     TIME_INPUT = 'time_input'
+    COST_HISTORY_CATEGORY_TYPE = 'time'
+    COST_CURRENT_CATEGORY_TYPE = 'AWS-Account'
+    PARAMS_INTERVAL = {'interval': 'daily'} 
+    PARAMS_TIME_INPUT = {'interval': 'daily', 'filters[]': 'time:select:{}'.format(TIME_INPUT)}
+    API_CALL = {'api_call': 'return'}
+    API_CALL_ERROR = {'error': 'Error message'} 
     
-    @patch('krux_cloud_health.cloud_health.get_stats')
-    @patch('krux_cloud_health.cloud_health.get_logger')
-    def setUp(self, mock_logger, mock_stats):
-        self.mock_logger = mock_logger
-        self.mock_stats = mock_stats
-        self.cloud_health = CloudHealth(CloudHealthTest.API_KEY, self.mock_logger, self.mock_stats)
+    def setUp(self):
+        self.cloud_health = get_cloud_health(args=MagicMock(api_key=CloudHealthTest.API_KEY))
 
     @patch('krux_cloud_health.cloud_health.get_stats')
     @patch('krux_cloud_health.cloud_health.get_logger')
@@ -58,64 +64,74 @@ class CloudHealthTest(unittest.TestCase):
 
     @patch('krux_cloud_health.cloud_health.get_stats')
     @patch('krux_cloud_health.cloud_health.get_logger')
-    @patch('krux_cloud_health.cloud_health.get_parser')
     @patch('krux_cloud_health.cloud_health.CloudHealth')
-    def test_get_cloud_health_all_args(self, mock_cloud_health, mock_parser, mock_logger, mock_stats):
+    def test_get_cloud_health_all_args(self, mock_cloud_health, mock_logger, mock_stats):
         """
-        Cloud Health Test: All arguments created and passed into CloudHealth if none are provided.
+        Cloud Health Test: All arguments into passed CloudHealth if provided.
         """
-        cloud_health = get_cloud_health()
+        mock_args = MagicMock(api_key=CloudHealthTest.API_KEY)
+        cloud_health = get_cloud_health(mock_args, mock_logger, mock_stats)
         mock_cloud_health.assert_called_once_with(
             api_key = CloudHealthTest.API_KEY,
-            logger=mock_logger(name=NAME),
-            stats=mock_stats(prefix=NAME),
+            logger=mock_logger,
+            stats=mock_stats,
             )
 
     def test_cost_history_time_input(self):
         """
-        Cloud Health Test: Cost history method properly passes in arguments to Get API call method.
-        with time_input provided
+        Cloud Health Test: Cost history method properly passes in arguments with time_input provided
+        to Get API call method.
         """
         self.cloud_health._get_api_call = MagicMock()
         self.cloud_health._get_data = MagicMock()
-        cost_history = self.cloud_health.cost_history(Interval['daily'], CloudHealthTest.TIME_INPUT)
+        cost_history = self.cloud_health.cost_history(CloudHealthTest.TIME_INTERVAL, CloudHealthTest.TIME_INPUT)
         self.cloud_health._get_api_call.assert_called_once_with(
             CloudHealthTest.COST_HISTORY_REPORT,
             CloudHealthTest.API_KEY,
-            {'interval': 'daily', 'filters[]': 'time:select:time_input'},
+            CloudHealthTest.PARAMS_TIME_INPUT,
             )
         self.cloud_health._get_data.assert_called_once_with(
-            self.cloud_health._get_api_call(CloudHealthTest.COST_HISTORY_REPORT,
+            self.cloud_health._get_api_call(
+                CloudHealthTest.COST_HISTORY_REPORT,
                 CloudHealthTest.API_KEY,
-                {'interval': 'daily', 'filters[]': 'time:select:time_input'}),
-                'time',
+                CloudHealthTest.PARAMS_TIME_INPUT),
+                CloudHealthTest.COST_HISTORY_CATEGORY_TYPE,
                 CloudHealthTest.TIME_INPUT
                 )
-
 
     def test_cost_history_no_time_input(self):
         """
         Cloud Health Test: Cost history method properly passes in arguments to Get API call method.
         without a specified time_input
         """
-        self.cloud_health._get_api_call = MagicMock()
-        cost_history = self.cloud_health.cost_history(Interval['daily'])
+        self.cloud_health._get_api_call = MagicMock(return_value=CloudHealthTest.API_CALL)
+        self.cloud_health._get_data = MagicMock()
+        cost_history = self.cloud_health.cost_history(CloudHealthTest.TIME_INTERVAL)
         self.cloud_health._get_api_call.assert_called_once_with(
             CloudHealthTest.COST_HISTORY_REPORT,
             CloudHealthTest.API_KEY,
-            {'interval': 'daily'},
+            CloudHealthTest.PARAMS_INTERVAL,
             )
+        self.cloud_health._get_data.assert_called_once_with(
+            CloudHealthTest.API_CALL,
+            CloudHealthTest.COST_HISTORY_CATEGORY_TYPE,
+            None)
 
     def test_cost_current(self):
         """
         Cloud Health Test: Cost current method properly passes in arguments to Get API call method.
         """
-        self.cloud_health._get_api_call = MagicMock()
+        self.cloud_health._get_api_call = MagicMock(return_value=CloudHealthTest.API_CALL)
+        self.cloud_health._get_data = MagicMock()
         cost_current = self.cloud_health.cost_current()
         self.cloud_health._get_api_call.assert_called_once_with(
             CloudHealthTest.COST_CURRENT_REPORT,
             CloudHealthTest.API_KEY
             )
+        self.cloud_health._get_data.assert_called_once_with(
+            CloudHealthTest.API_CALL,
+            CloudHealthTest.COST_CURRENT_CATEGORY_TYPE,
+            None)
 
     @patch('krux_cloud_health.cloud_health.pprint.pformat')
     @patch('krux_cloud_health.cloud_health.requests')
@@ -123,21 +139,29 @@ class CloudHealthTest(unittest.TestCase):
         """
         Cloud Health Test: Get API call method calls API with valid report and API key.
         """
-        api_call = {'api_call': 'return'}
-        mock_request.get.return_value.json.return_value = api_call
+        self.cloud_health.logger = MagicMock()
+        mock_request.get.return_value.json.return_value = CloudHealthTest.API_CALL
         get_api_call = self.cloud_health._get_api_call(CloudHealthTest.COST_HISTORY_REPORT, CloudHealthTest.API_KEY)
-        mock_pprint.assert_called_once_with(api_call)
-        self.cloud_health.logger.debug.assert_called_once_with(mock_pprint(api_call))
+        mock_request.get.assert_called_once_with(
+            CloudHealthTest.COST_HISTORY_URI,
+            params=CloudHealthTest.URI_ARGS_NO_PARAMS
+            )
+        mock_pprint.assert_called_once_with(CloudHealthTest.API_CALL)
+        self.cloud_health.logger.debug.assert_called_once_with(mock_pprint(CloudHealthTest.API_CALL))
+        self.assertEqual(get_api_call, CloudHealthTest.API_CALL)
 
     @patch('krux_cloud_health.cloud_health.requests')
     def test_get_api_call_error(self, mock_request):
         """
         Cloud Health Test: Get API call method throws ValueError if API returns an error.
         """
-        api_call = {'error': 'Error message'}
-        mock_request.get.return_value.json.return_value = api_call
-        with self.assertRaises(ValueError):
-            get_api_call = self.cloud_health._get_api_call(CloudHealthTest.COST_HISTORY_REPORT, CloudHealthTest.API_KEY)
+        mock_request.get.return_value.json.return_value = CloudHealthTest.API_CALL_ERROR
+        self.cloud_health.logger = MagicMock()
+        with self.assertRaises(ValueError) as ve:
+            get_api_call = self.cloud_health._get_api_call(
+                CloudHealthTest.COST_HISTORY_REPORT,
+                CloudHealthTest.API_KEY)
+        self.assertEqual(ve.exception.message, CloudHealthTest.API_CALL_ERROR.get('error'))
 
     def test_get_data(self):
         """
