fa25a52ab51e9a2733685520b1c746f96946229c
Peter Han <phan@krux.com>
2016-09-02 12:57:55 -0700

    Incrementing patch version for bug fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from __future__ import absolute_import
 from setuptools import setup, find_packages
 
 # We use the version to construct the DOWNLOAD_URL.
-VERSION  = '0.1.1'
+VERSION  = '0.1.2'
 
 # URL to the repository on Github.
 REPO_URL = 'https://github.com/krux/python-krux-cloud-health'
