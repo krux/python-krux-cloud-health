2edf2a4436a2904caf3e3afdfcac7b0b1328ba07
Peter Han <phan@krux.com>
2017-01-10 12:42:03 -0800

    Unit test for --date-format

--- tests/bin/cloud_health_to_graphite_test.py
+++ tests/bin/cloud_health_to_graphite_test.py
@@ -10,7 +10,7 @@
 from __future__ import absolute_import
 import unittest
 import pprint
-from datetime import datetime
+from datetime import datetime, timedelta
 import calendar
 from StringIO import StringIO
 import re
@@ -39,17 +39,26 @@ class CloudHealthAPITest(unittest.TestCase):
     REPORT_NAME_ARG = 'fake report'
     REPORT_NAME = re.sub('[ \.]+', '_', REPORT_NAME_ARG)
     SET_DATE = '2016-05-01'
+    DATE_FORMAT = '%Y-%m-%d %H:%M'
 
+    _DEFAULT_DATE_FORMAT = '%Y-%m-%d'
     _STDOUT_FORMAT = 'cloud_health.{env}.{report_name}.{category} {cost} {date}\n'
 
     @staticmethod
-    def _get_cloud_health_return(report_id, category=None):
+    def _get_cloud_health_return(report_id, category=None, date_format=_DEFAULT_DATE_FORMAT):
+        """
+        Creates a fake data to mock Cloud Health API
+        """
         result = {
             'Total': {'key1': 'value1', 'key2': None},
-            '2016-05-01': {'key1': 'value1', 'key2': None},
-            '2016-06-01': {'key1': 'value1', 'key2': None},
-            '2016-07-01': {'key1': 'value1', 'key2': None},
         }
+        dt = datetime.today()
+        for index in range(0, 11):
+            result[dt.strftime(date_format)] = {
+                'key1': 'value1',
+                'key2': None,
+            }
+            dt = dt - timedelta(days=30)
 
         if category is None:
             return result
@@ -83,11 +92,13 @@ class CloudHealthAPITest(unittest.TestCase):
         self.assertIn('report_id', self.app.args)
         self.assertIn('report_name', self.app.args)
         self.assertIn('set_date', self.app.args)
+        self.assertIn('date_format', self.app.args)
 
         self.assertEqual(self.API_KEY, self.app.args.api_key)
         self.assertEqual(self.REPORT_ID, self.app.args.report_id)
         self.assertEqual(self.REPORT_NAME_ARG, self.app.args.report_name)
         self.assertIsNone(self.app.args.set_date)
+        self.assertEqual(self._DEFAULT_DATE_FORMAT, self.app.args.date_format)
 
     def test_run_error(self):
         error = ValueError('Error message')
@@ -145,6 +156,43 @@ class CloudHealthAPITest(unittest.TestCase):
 
         for date, values in iteritems(CloudHealthAPITest._get_cloud_health_return(self.REPORT_ID, self.SET_DATE)):
             date = int(calendar.timegm(datetime.strptime(self.SET_DATE, '%Y-%m-%d').utctimetuple()))
+
+            for category, cost in iteritems(values):
+                if cost is not None:
+                    prints += self._STDOUT_FORMAT.format(
+                        env=self.app.args.stats_environment,
+                        report_name=self.REPORT_NAME,
+                        category=category,
+                        cost=cost,
+                        date=date,
+                    )
+
+        self.assertEqual(prints, mock_stdout.getvalue())
+
+    @patch('sys.argv', ['prog', API_KEY, REPORT_ID_ARG, '--report-name', REPORT_NAME_ARG, '--date-format', DATE_FORMAT])
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_run_with_set_date(self, mock_stdout):
+        app = Application()
+        # Create a lambda function that calls _get_cloud_health_return() with CloudHealthAPITest.DATE_FORMAT
+        # This is because side_effect can only take a function pointer
+        app.cloud_health.get_custom_report = MagicMock(
+            side_effect=lambda report_id, category: CloudHealthAPITest._get_cloud_health_return(
+                report_id=report_id, date_format=CloudHealthAPITest.DATE_FORMAT,
+            )
+        )
+
+        app.run()
+
+        prints = ''
+
+        api_data = CloudHealthAPITest._get_cloud_health_return(
+            self.REPORT_ID, date_format=CloudHealthAPITest.DATE_FORMAT,
+        )
+        for date, values in iteritems(api_data):
+            if date is 'Total':
+                continue
+
+            date = int(calendar.timegm(datetime.strptime(date, self.DATE_FORMAT).utctimetuple()))
 
             for category, cost in iteritems(values):
                 if cost is not None:
